76a08b83515049888c0c2c33d553bb773cd22ed7
Yaseen Mohammed <yasien202020@gmail.com>
2025-04-28 23:41:28 +0300

    add CRUD methods to StateRepository and its implementation

--- src/main/kotlin/data/repository/StateRepositoryImpl.kt
+++ src/main/kotlin/data/repository/StateRepositoryImpl.kt
@@ -1,5 +1,25 @@
 package org.example.data.repository
 
+import org.example.data.DataProvider
+import org.example.entity.StateEntity
 import org.example.logic.repository.StateRepository
 
-class StateRepositoryImpl: StateRepository
+class StateRepositoryImpl(
+    private val dataProvider: DataProvider<StateEntity>
+): StateRepository {
+    override fun createState(state: StateEntity): String {
+        TODO("Not yet implemented")
+    }
+
+    override fun updateState(stateId: StateEntity, newState: StateEntity) {
+        TODO("Not yet implemented")
+    }
+
+    override fun deleteState(stateId: StateEntity): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun ensureStateCsvExists(filePath: String): Boolean {
+        TODO("Not yet implemented")
+    }
+}
--- src/main/kotlin/logic/repository/StateRepository.kt
+++ src/main/kotlin/logic/repository/StateRepository.kt
@@ -1,3 +1,10 @@
 package org.example.logic.repository
 
-interface StateRepository
+import org.example.entity.StateEntity
+
+interface StateRepository {
+    fun createState(state: StateEntity): String
+    fun updateState(stateId: StateEntity, newState: StateEntity)
+    fun deleteState(stateId: StateEntity): Boolean
+    fun ensureStateCsvExists(filePath: String) : Boolean
+}
