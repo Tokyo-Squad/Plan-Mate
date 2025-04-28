6bd4bb0170fb8c5e2d558be865a3e7cc0772b732
Kareem Mohamed <info.kareemmohamed@gmail.com>
2025-04-28 12:09:25 +0300

    add user entity.

---
+++ src/main/kotlin/entity/UserEntity.kt
@@ -0,0 +1,15 @@
+package org.example.entity
+
+import java.util.UUID
+
+data class UserEntity(
+    val id: UUID = UUID.randomUUID(),
+    val userName: String,
+    val hashedPassword: String,
+    val type: UserType
+)
+
+enum class UserType {
+    ADMIN, MATE
+}
+
