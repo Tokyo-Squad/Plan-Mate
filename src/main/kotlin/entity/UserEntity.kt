3fba9e9a1e7d7f5d7a066ec43d8bd384bc715fbb
Kareem Mohamed <info.kareemmohamed@gmail.com>
2025-04-28 21:03:01 +0300

    change name of hashed password to password.

--- src/main/kotlin/entity/UserEntity.kt
+++ src/main/kotlin/entity/UserEntity.kt
@@ -5,7 +5,7 @@ import java.util.UUID
 data class UserEntity(
     val id: UUID = UUID.randomUUID(),
     val userName: String,
-    val hashedPassword: String,
+    val password: String,
     val type: UserType
 )
 
