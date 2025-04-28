b9572dfdf5deb1ab9f95c2b3baefb1aed2c0c11e
Yaseen Mohammed <yasien202020@gmail.com>
2025-04-28 22:31:25 +0300

    add test method stubs for AuditLogRepositoryImpl

--- src/test/kotlin/data/repository/AuditLogRepositoryImplTest.kt
+++ src/test/kotlin/data/repository/AuditLogRepositoryImplTest.kt
@@ -6,6 +6,7 @@ import org.example.data.repository.AuditLogRepositoryImpl
 import org.example.entity.AuditLogEntity
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.BeforeEach
+import kotlin.test.Test
 
 
 class AuditLogRepositoryImplTest {
@@ -15,6 +16,36 @@ class AuditLogRepositoryImplTest {
     @BeforeEach
     fun setUp() {
         auditLogRepository = AuditLogRepositoryImpl(dataProvider)
+    }
+
+    @Test
+    fun `should create a new audit log when creating a new project or task`() {
+        TODO("Implementation of test case")
+    }
+
+    @Test
+    fun `should save to audit log when making changes to a project or task`() {
+        TODO("Implementation of test case")
+    }
+
+    @Test
+    fun `should retrieve audit logs filtered by project ID`() {
+        TODO("Implementation of test case")
+    }
+
+    @Test
+    fun `should retrieve audit logs filtered by task ID`() {
+        TODO("Implementation of test case")
+    }
+
+    @Test
+    fun `should not allow saving a log with missing mandatory fields`() {
+        TODO("Implementation of test case")
+    }
+
+    @Test
+    fun `should throw exception when audit log is empty`() {
+        TODO("Implementation of test case")
     }
 
 }
