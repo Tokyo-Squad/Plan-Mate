c8287b016fa312627ed2e77f6228fe6d2f4377d9
mohamedebrahem13 <mohamedebrahem1447@gmail.com>
2025-04-29 17:54:00 +0300

    fix app module always use the same file name audit_logs.csv

--- src/main/kotlin/di/AppModule.kt
+++ src/main/kotlin/di/AppModule.kt
@@ -1,18 +1,19 @@
 package org.example.di
 
 import data.csvfile.*
+import org.koin.core.qualifier.named
 import org.koin.dsl.module
 
 val appModule = module {
-    single { "projects.csv" }
-    single { "states.csv" }
-    single { "tasks.csv" }
-    single { "users.csv" }
-    single { "audit_logs.csv" }
+    single(named("projects")) { "projects.csv" }
+    single(named("states")) { "states.csv" }
+    single(named("tasks")) { "tasks.csv" }
+    single(named("users")) { "users.csv" }
+    single(named("auditLogs")) { "audit_logs.csv" }
 
-    single { ProjectCsvImpl(get()) }
-    single { StateCsvImpl(get()) }
-    single { TaskCsvImpl(get()) }
-    single { UserCsvImpl(get()) }
-    single { AuditLogCsvImpl(get()) }
+    single { ProjectCsvImpl(get(named("projects"))) }
+    single { StateCsvImpl(get(named("states"))) }
+    single { TaskCsvImpl(get(named("tasks"))) }
+    single { UserCsvImpl(get(named("users"))) }
+    single { AuditLogCsvImpl(get(named("auditLogs"))) }
 }
