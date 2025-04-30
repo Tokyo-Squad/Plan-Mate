374577b07b3e986d4d4293d647ee788ca29ff58e
Kareem Mohamed <info.kareemmohamed@gmail.com>
2025-04-30 13:32:40 +0300

    add update user use case.

---
+++ src/main/kotlin/logic/usecase/user/UpdateUserUseCase.kt
@@ -0,0 +1,18 @@
+package org.example.logic.usecase.user
+
+import org.example.entity.UserEntity
+import org.example.entity.UserType
+import org.example.logic.repository.UserRepository
+
+class UpdateUserUseCase(
+    private val userRepository: UserRepository
+) {
+    operator fun invoke(user: UserEntity, currentUser: UserEntity): Result<Unit> {
+        if (currentUser.type == UserType.MATE) {
+            return Result.failure(IllegalStateException("MATE users are not allowed to update users"))
+        }
+        return runCatching {
+            userRepository.update(user).getOrThrow()
+        }
+    }
+}
