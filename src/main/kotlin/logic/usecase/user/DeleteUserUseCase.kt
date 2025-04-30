be226ee13d8f9db74177a24dc3578c052fe0d550
Kareem Mohamed <info.kareemmohamed@gmail.com>
2025-04-30 13:30:06 +0300

    add delete user use case.

---
+++ src/main/kotlin/logic/usecase/user/DeleteUserUseCase.kt
@@ -0,0 +1,23 @@
+package org.example.logic.usecase.user
+
+import org.example.entity.UserEntity
+import org.example.entity.UserType
+import org.example.logic.repository.UserRepository
+import java.util.UUID
+
+class DeleteUserUseCase(
+    private val userRepository: UserRepository
+) {
+
+    operator fun invoke(
+        id: UUID,
+        currentUser: UserEntity
+    ): Result<Unit> {
+        if (currentUser.type == UserType.MATE) {
+            return Result.failure(IllegalStateException("MATE users are not allowed to delete users"))
+        }
+        return runCatching {
+            userRepository.delete(id).getOrThrow()
+        }
+    }
+}
