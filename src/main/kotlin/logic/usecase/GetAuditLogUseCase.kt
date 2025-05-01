b1ee96009270e31565c3f80267935cebc2c47684
mena <mena.rizkalla1@gmail.com>
2025-05-01 14:38:33 +0300

    introduce entity type filtering for audit logs

--- src/main/kotlin/logic/usecase/GetAuditLogUseCase.kt
+++ src/main/kotlin/logic/usecase/GetAuditLogUseCase.kt
@@ -1,19 +1,23 @@
 package org.example.logic.usecase
 
 import org.example.entity.AuditLogEntity
+import org.example.entity.AuditedEntityType
 import org.example.logic.repository.AuditLogRepository
 
 class GetAuditLogUseCase(
     private val auditLogRepository: AuditLogRepository
 ) {
 
-    operator fun invoke(id: Int): Result<List<AuditLogEntity>> {
-        return getAuditLogs(id)
+    operator fun invoke(id: Int, entityType: AuditedEntityType): Result<List<AuditLogEntity>> {
+        return getAuditLogs(id,entityType)
     }
 
-    private fun getAuditLogs(id: Int): Result<List<AuditLogEntity>> {
+    private fun getAuditLogs(id: Int, entityType: AuditedEntityType): Result<List<AuditLogEntity>> {
         return try {
-            Result.success(auditLogRepository.getTaskHistory(id))
+            when (entityType) {
+                AuditedEntityType.PROJECT -> Result.success(auditLogRepository.getProjectHistory(id))
+                AuditedEntityType.TASK -> Result.success(auditLogRepository.getTaskHistory(id))
+            }
         } catch (e: NoSuchElementException) {
             Result.failure(e)
         }
