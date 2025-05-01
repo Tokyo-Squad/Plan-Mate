f4b6487fc8a1e0a9e8c08c8cdffed5a1afd165bd
Yaseen Mohammed <yasien202020@gmail.com>
2025-05-01 17:28:08 +0300

    add InvalidStateIdException and handle it in AddAuditLogUseCase

--- src/main/kotlin/logic/usecase/audit/AddAuditLogUseCase.kt
+++ src/main/kotlin/logic/usecase/audit/AddAuditLogUseCase.kt
@@ -2,11 +2,17 @@ package org.example.logic.usecase.audit
 
 import org.example.entity.AuditLogEntity
 import org.example.logic.repository.AuditLogRepository
+import org.example.utils.PlanMatException.InvalidStateIdException
 
 class AddAuditLogUseCase(
     private val auditLogRepository: AuditLogRepository
 ) {
-    operator fun invoke(auditLogEntity: AuditLogEntity) {
-        auditLogRepository.addAudit(auditLogEntity)
+    operator fun invoke(auditLogEntity: AuditLogEntity): Result<Boolean> {
+        return try {
+            auditLogRepository.addAudit(auditLogEntity)
+            Result.success(true)
+        } catch (e: InvalidStateIdException) {
+            Result.failure(e)
+        }
     }
 }
