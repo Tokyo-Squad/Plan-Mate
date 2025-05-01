42a698cc1a03e4bac9fe5695c22095db096fb918
Kareem Mohamed <info.kareemmohamed@gmail.com>
2025-05-01 05:47:12 +0300

    add di for user use cases.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,10 +1,11 @@
 package org.example
 
 import org.example.di.appModule
+import org.example.di.useCaseModule
 import org.koin.core.context.startKoin
 
 fun main() {
     startKoin {
-        modules(appModule)
+        modules(appModule, useCaseModule)
     }
 }
