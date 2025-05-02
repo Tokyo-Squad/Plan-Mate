4b0db631f1ad56417c4e256b8497c878c53c2d37
Kareem Mohamed <info.kareemmohamed@gmail.com>
2025-05-02 17:07:41 +0300

    add create task use case.

--- src/main/kotlin/logic/usecase/task/CreateTaskUseCase.kt
+++ src/main/kotlin/logic/usecase/task/CreateTaskUseCase.kt
@@ -4,10 +4,13 @@ import org.example.entity.TaskEntity
 import org.example.logic.repository.TaskRepository
 import java.util.UUID
 
-class CreateTaskUseCase (
+class CreateTaskUseCase(
     private val taskRepository: TaskRepository,
-){
-    operator fun invoke(task: TaskEntity,currentUserId: UUID): Result<Unit> {
-        return Result.failure(Exception("CreateTaskUseCase"))
+) {
+    operator fun invoke(task: TaskEntity, currentUserId: UUID): Result<Unit> {
+        if (task.title.isBlank()) {
+            return Result.failure(IllegalArgumentException("title cannot be empty"))
+        }
+        return runCatching { taskRepository.create(task, currentUserId).getOrThrow() }
     }
 }
