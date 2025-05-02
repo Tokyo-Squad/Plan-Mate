c56288be441b1a2320ae648484b2c52e196c1335
Kareem Mohamed <info.kareemmohamed@gmail.com>
2025-05-02 19:28:27 +0300

    update task repo for remove audited type.

--- src/main/kotlin/logic/repository/TaskRepository.kt
+++ src/main/kotlin/logic/repository/TaskRepository.kt
@@ -9,6 +9,6 @@ interface TaskRepository {
     fun delete(id: UUID,currentUserId:UUID): Result<Unit>
     fun getTaskById(id: UUID): Result<TaskEntity>
     fun getTasksByProjectId(id: UUID): Result<List<TaskEntity>>
-    fun update(task: TaskEntity, currentUserId: AuditedEntityType, userId: UUID?): Result<Unit>
+    fun update(task: TaskEntity, currentUserId: UUID): Result<Unit>
 
 }
