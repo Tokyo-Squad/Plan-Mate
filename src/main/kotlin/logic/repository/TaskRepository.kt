6f886c008b40db839cd94e21b2fa4b73d843c6df
ali thaar <Alithaar98.at@gmail.com>
2025-05-02 18:13:39 +0300

    Modify some things in the TaskRepository

--- src/main/kotlin/logic/repository/TaskRepository.kt
+++ src/main/kotlin/logic/repository/TaskRepository.kt
@@ -1,13 +1,14 @@
 package org.example.logic.repository
 
+import org.example.entity.AuditedEntityType
 import org.example.entity.TaskEntity
 import java.util.UUID
 
 interface TaskRepository {
     fun create(task: TaskEntity,currentUserId:UUID): Result<Unit>
-    fun update(task: TaskEntity,currentUserId:UUID): Result<Unit>
     fun delete(id: UUID,currentUserId:UUID): Result<Unit>
     fun getTaskById(id: UUID): Result<TaskEntity>
     fun getTasksByProjectId(id: UUID): Result<List<TaskEntity>>
+    fun update(task: TaskEntity, currentUserId: AuditedEntityType, userId: UUID?): Result<Unit>
 
 }
