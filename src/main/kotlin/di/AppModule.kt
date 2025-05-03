238a86ada06a25fcb82b916d97c409272a111436
Yaseen Mohammed <129616611+Yaseen20022@users.noreply.github.com>
2025-05-03 15:10:07 +0300

    Fix/koin module (#31)
    * fix DI modules to streamline repository and use case setup
    
    * removed unused imports
    
    * refactor dependency declarations in AppModule.

--- src/main/kotlin/di/AppModule.kt
+++ src/main/kotlin/di/AppModule.kt
@@ -1,6 +1,8 @@
 package org.example.di
 
 import data.csvfile.*
+import org.example.data.AuthProvider
+import org.example.data.DataProvider
 import org.example.data.csvfile.AuthProviderImpl
 import org.example.data.repository.AuditLogRepositoryImpl
 import org.example.data.repository.AuthenticationRepositoryImpl
@@ -8,6 +10,11 @@ import org.example.data.repository.ProjectRepositoryImpl
 import org.example.data.repository.StateRepositoryImpl
 import org.example.data.repository.TaskRepositoryImpl
 import org.example.data.repository.UserRepositoryImpl
+import org.example.entity.AuditLogEntity
+import org.example.entity.ProjectEntity
+import org.example.entity.StateEntity
+import org.example.entity.TaskEntity
+import org.example.entity.UserEntity
 import org.example.logic.repository.AuditLogRepository
 import org.example.logic.repository.AuthenticationRepository
 import org.example.logic.repository.ProjectRepository
@@ -17,25 +24,27 @@ import org.example.logic.repository.UserRepository
 import org.koin.core.qualifier.named
 import org.koin.dsl.module
 
-val appModule = module {
-    single(named("projects")) { "projects.csv" }
-    single(named("states")) { "states.csv" }
-    single(named("tasks")) { "tasks.csv" }
-    single(named("users")) { "users.csv" }
-    single(named("auditLogs")) { "audit_logs.csv" }
-    single(named("auth")) { "auth.csv" }
+val appModule = module{
 
-    single { ProjectCsvImpl((get(named("projects")))) }
-    single { AuditLogCsvImpl(get(named("auditLogs"))) }
-    single { TaskCsvImpl(get(named("tasks"))) }
-    single { UserCsvImpl(get(named("users"))) }
-    single { StateCsvImpl(get(named("states"))) }
-    single { AuthProviderImpl(get(named("auth"))) }
+        single(named("projects")) { "projects.csv" }
+        single(named("states")) { "states.csv" }
+        single(named("tasks")) { "tasks.csv" }
+        single(named("users")) { "users.csv" }
+        single(named("auditLogs")) { "audit_logs.csv" }
+        single(named("auth")) { "auth.csv" }
 
-    single<AuditLogRepository> { AuditLogRepositoryImpl(get()) }
-    single<AuthenticationRepository> { AuthenticationRepositoryImpl(get(), get(), get()) }
-    single<ProjectRepository> { ProjectRepositoryImpl(get(), get()) }
-    single<StateRepository> { StateRepositoryImpl(get()) }
-    single<TaskRepository> { TaskRepositoryImpl(get(), get(), get()) }
-    single<UserRepository> { UserRepositoryImpl(get()) }
-}
+        single<DataProvider<ProjectEntity>> { ProjectCsvImpl(get(named("projects"))) }
+        single<DataProvider<StateEntity>> { StateCsvImpl(get(named("states"))) }
+        single<DataProvider<TaskEntity>> { TaskCsvImpl(get(named("tasks"))) }
+        single<DataProvider<UserEntity>> { UserCsvImpl(get(named("users"))) }
+        single<DataProvider<AuditLogEntity>> { AuditLogCsvImpl(get(named("auditLogs"))) }
+
+        single<AuthProvider> { AuthProviderImpl(get(named("auth"))) }
+
+        single<AuditLogRepository> { AuditLogRepositoryImpl(get()) }
+        single<AuthenticationRepository> { AuthenticationRepositoryImpl(get(), get(), get()) }
+        single<ProjectRepository> { ProjectRepositoryImpl(get(), get()) }
+        single<StateRepository> { StateRepositoryImpl(get()) }
+        single<TaskRepository> { TaskRepositoryImpl(get(), get(), get()) }
+        single<UserRepository> { UserRepositoryImpl(get()) }
+    }
