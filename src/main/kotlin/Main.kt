381622fd49c11e688f8f63ec3a90559f9d767cc7
Mostafa Mohamed Fahmy <Mostafa0Mohamed2002@gmail.com>
2025-05-03 18:55:54 +0300

    wire up Koin UI module and launch interactive console

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,11 +1,22 @@
 package org.example
 
 import org.example.di.appModule
+import org.example.di.uiModule
 import org.example.di.useCaseModule
+import org.example.presentation.PlanMateConsoleUI
 import org.koin.core.context.startKoin
+import org.koin.java.KoinJavaComponent.getKoin
+
 
 fun main() {
     startKoin {
-        modules(appModule, useCaseModule)
+        modules(
+            appModule,
+            useCaseModule,
+            uiModule
+        )
     }
+//    CreateDefaultAdminUseCase(getKoin().get(named("userDataProvider")))
+    val console: PlanMateConsoleUI = getKoin().get()
+    console.start()
 }
