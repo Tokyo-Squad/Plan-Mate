519388718a39daae395a881085e468e54c3bfee0
Mostafa Mohamed Fahmy <Mostafa0Mohamed2002@gmail.com>
2025-05-03 18:55:16 +0300

    modularize and update DI bindings for UI, use cases, and repositories

--- src/main/kotlin/di/AppModule.kt
+++ src/main/kotlin/di/AppModule.kt
@@ -1,41 +1,68 @@
 package org.example.di
 
 import data.csvfile.*
+import org.example.data.AuthProvider
+import org.example.data.DataProvider
 import org.example.data.csvfile.AuthProviderImpl
-import org.example.data.repository.AuditLogRepositoryImpl
-import org.example.data.repository.AuthenticationRepositoryImpl
-import org.example.data.repository.ProjectRepositoryImpl
-import org.example.data.repository.StateRepositoryImpl
-import org.example.data.repository.TaskRepositoryImpl
-import org.example.data.repository.UserRepositoryImpl
-import org.example.logic.repository.AuditLogRepository
-import org.example.logic.repository.AuthenticationRepository
-import org.example.logic.repository.ProjectRepository
-import org.example.logic.repository.StateRepository
-import org.example.logic.repository.TaskRepository
-import org.example.logic.repository.UserRepository
+import org.example.data.repository.*
+import org.example.entity.*
+import org.example.logic.repository.*
 import org.koin.core.qualifier.named
 import org.koin.dsl.module
 
 val appModule = module {
+    // CSV filenames
     single(named("projects")) { "projects.csv" }
     single(named("states")) { "states.csv" }
     single(named("tasks")) { "tasks.csv" }
     single(named("users")) { "users.csv" }
     single(named("auditLogs")) { "audit_logs.csv" }
     single(named("auth")) { "auth.csv" }
-
-    single { ProjectCsvImpl((get(named("projects")))) }
-    single { AuditLogCsvImpl(get(named("auditLogs"))) }
-    single { TaskCsvImpl(get(named("tasks"))) }
-    single { UserCsvImpl(get(named("users"))) }
-    single { StateCsvImpl(get(named("states"))) }
-    single { AuthProviderImpl(get(named("auth"))) }
-
-    single<AuditLogRepository> { AuditLogRepositoryImpl(get()) }
-    single<AuthenticationRepository> { AuthenticationRepositoryImpl(get(), get(), get()) }
-    single<ProjectRepository> { ProjectRepositoryImpl(get(), get()) }
-    single<StateRepository> { StateRepositoryImpl(get()) }
-    single<TaskRepository> { TaskRepositoryImpl(get(), get(), get()) }
-    single<UserRepository> { UserRepositoryImpl(get()) }
+    // Generic data providers
+    single<DataProvider<ProjectEntity>>(named("projectDataProvider")) { ProjectCsvImpl(get(named("projects"))) }
+    single<DataProvider<StateEntity>>(named("stateDataProvider")) { StateCsvImpl(get(named("states"))) }
+    single<DataProvider<TaskEntity>>(named("taskDataProvider")) { TaskCsvImpl(get(named("tasks"))) }
+    single<DataProvider<UserEntity>>(named("userDataProvider")) { UserCsvImpl(get(named("users"))) }
+    single<DataProvider<AuditLogEntity>>(named("auditDataProvider")) { AuditLogCsvImpl(get(named("auditLogs"))) }
+    // Auth provider
+    single<AuthProvider> { AuthProviderImpl(get(named("auth"))) }
+    // Repositories
+    single<AuditLogRepository> { AuditLogRepositoryImpl(get(qualifier = named("auditDataProvider"))) }
+    single<AuthenticationRepository> {
+        AuthenticationRepositoryImpl(
+            get(),
+            get(),
+            get(qualifier = named("userDataProvider"))
+        )
+    }
+    single<ProjectRepository> {
+        ProjectRepositoryImpl(
+            get(
+                qualifier = named("projectDataProvider")
+            ), get(
+                qualifier = named("auditDataProvider")
+            )
+        )
+    }
+    single<StateRepository> {
+        StateRepositoryImpl(
+            get(
+                qualifier = named("stateDataProvider")
+            )
+        )
+    }
+    single<TaskRepository> {
+        TaskRepositoryImpl(
+            get(), get(
+                qualifier = named("taskDataProvider")
+            ), get()
+        )
+    }
+    single<UserRepository> {
+        UserRepositoryImpl(
+            get(
+                qualifier = named("userDataProvider")
+            )
+        )
+    }
 }
