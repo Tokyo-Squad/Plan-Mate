6b318ed6d26a06760bbb625ad6f2b224d300a19b
Yaseen Mohammed <yasien202020@gmail.com>
2025-05-06 21:39:48 +0300

    added AuditLogMongoDbImpl implementation and used MongoExceptionHandler

--- src/main/kotlin/data/mongo/AuditLogMongoDbImpl.kt
+++ src/main/kotlin/data/mongo/AuditLogMongoDbImpl.kt
@@ -1,37 +1,51 @@
 package org.example.data.mongo
 
-import com.mongodb.kotlin.client.coroutine.MongoClient
-import com.mongodb.kotlin.client.coroutine.MongoCollection
-import kotlinx.coroutines.flow.toList
+
 import org.example.data.DataProvider
 import org.example.entity.AuditLogEntity
-import java.util.UUID
-import java.util.*
+import org.example.utils.MongoExceptionHandler
+import com.mongodb.kotlin.client.coroutine.MongoClient
+import com.mongodb.kotlin.client.coroutine.MongoCollection
 import com.mongodb.client.model.Filters
 import kotlinx.coroutines.flow.firstOrNull
-
+import kotlinx.coroutines.flow.toList
+import java.util.UUID
 
 class AuditLogMongoDbImpl(
-    private val mongoClient: MongoClient
-): DataProvider<AuditLogEntity>  {
-    private val auditLogCollection: MongoCollection<AuditLogEntity> = mongoClient.getDatabase().getCollection("audit-logs")
+    mongoClient: MongoClient
+) : DataProvider<AuditLogEntity> {
+
+    private val collection: MongoCollection<AuditLogEntity> =
+        mongoClient.getDatabase("PlanMate").getCollection("audit-log", AuditLogEntity::class.java)
+
     override suspend fun add(item: AuditLogEntity) {
-        TODO("Not yet implemented")
+        MongoExceptionHandler.handleOperation("adding audit log") {
+            collection.insertOne(item)
+        }
     }
 
     override suspend fun get(): List<AuditLogEntity> {
-        TODO("Not yet implemented")
+        return MongoExceptionHandler.handleOperation("fetching all audit logs") {
+            collection.find().toList()
+        }
     }
 
     override suspend fun getById(id: UUID): AuditLogEntity? {
-        TODO("Not yet implemented")
+        return MongoExceptionHandler.handleOperation("fetching audit log by ID") {
+            collection.find(Filters.eq("_id", id)).firstOrNull()
+        }
     }
 
     override suspend fun update(item: AuditLogEntity) {
-        TODO("Not yet implemented")
+        MongoExceptionHandler.handleOperation("updating audit log") {
+            val filter = Filters.eq("_id", item.id)
+            collection.replaceOne(filter, item)
+        }
     }
 
     override suspend fun delete(id: UUID) {
-        TODO("Not yet implemented")
+        MongoExceptionHandler.handleOperation("deleting audit log") {
+            collection.deleteOne(Filters.eq("_id", id))
+        }
     }
 }
