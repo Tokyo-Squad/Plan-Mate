e866b16477c9ff694d01b3f5f473d265195a7fd7
ali thaar <Alithaar98.at@gmail.com>
2025-05-07 18:50:33 +0300

    class GetStateByIdUseCaseTest

--- src/test/kotlin/logic/usecase/state/GetStateByIdUseCaseTest.kt
+++ src/test/kotlin/logic/usecase/state/GetStateByIdUseCaseTest.kt
@@ -1,52 +1,56 @@
 package logic.usecase.state
 
-
 import com.google.common.truth.Truth.assertThat
 import fakeData.StateFakeData
-import io.mockk.every
+import io.mockk.coEvery
+import io.mockk.coVerify
 import io.mockk.mockk
+import kotlinx.coroutines.test.runTest
 import org.example.logic.repository.StateRepository
 import org.example.logic.usecase.GetStateByIdUseCase
 import org.example.utils.PlanMatException
 import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
 import java.util.UUID
-import kotlin.test.Test
 
 class GetStateByIdUseCaseTest {
     private lateinit var repo: StateRepository
     private lateinit var useCase: GetStateByIdUseCase
     private val fake = StateFakeData()
 
-    @BeforeEach fun setUp() {
+    @BeforeEach
+    fun setUp() {
         repo = mockk()
         useCase = GetStateByIdUseCase(repo)
     }
 
-
-    @Test fun `invoke returns state when valid id is entered`() {
-        //Given
+    @Test
+    fun `invoke returns state when valid id is entered`() = runTest {
+        // Given
         val id = UUID.randomUUID()
         val expected = fake.createState(id = id)
-        every { repo.getStateById(id) } returns Result.success(expected)
+        coEvery { repo.getStateById(id) } returns Result.success(expected)
 
-        //When
+        // When
         val result = useCase(id)
 
-        //Then
+        // Then
         assertThat(result.isSuccess).isTrue()
         assertThat(result.getOrNull()).isEqualTo(expected)
+        coVerify(exactly = 1) { repo.getStateById(id) }
     }
 
-    @Test fun `invoke returns failure when repository throws ItemNotFoundException`() {
-        //Given
+    @Test
+    fun `invoke returns failure when repository throws ItemNotFoundException`() = runTest {
+        // Given
         val id = UUID.randomUUID()
         val ex = PlanMatException.ItemNotFoundException("State with ID $id not found")
-        every { repo.getStateById(id) } throws ex
+        coEvery { repo.getStateById(id) } throws ex
 
-        //When
+        // When
         val result = useCase(id)
 
-        //Then
+        // Then
         assertThat(result.isFailure).isTrue()
         assertThat(result.exceptionOrNull()).isSameInstanceAs(ex)
     }
